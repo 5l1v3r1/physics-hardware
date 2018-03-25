670b9751f4175450c6e228ed4dfd3b36687e40ba
Tristan Granier <graniet75@gmail.com>
2018-03-25 13:14:19 +0200

    BLE device

--- core/command/bluetooth.go
+++ core/command/bluetooth.go
@@ -42,13 +42,11 @@ func onStateChanged(device gatt.Device, s gatt.State) {
 }
 
 func onPeripheralDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
-	b, err := NewiBeacon(a.ManufacturerData)
-	if err == nil {
+	
 		fmt.Println("UUID: ", p.ID())
 		fmt.Println("Major: ", p)
 		fmt.Println("Minor: ", a)
 		fmt.Println("RSSI: ", rssi)
-	}
 }
 
 func ReadDevices() {
